7de6df09cde317f0a18161d2b5141b33c66dcd1c
Dowland Aiello <mitsukomegumii@gmail.com>
2019-05-28 20:52:57 -0400

    go-pseudosub: Wrote NewPseudosub test.

--- pseudosub_test.go
+++ pseudosub_test.go
@@ -4,14 +4,53 @@
 // own routing solutions (e.g. kadDHT).
 package pseudosub
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/libp2p/go-libp2p"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	routed "github.com/libp2p/go-libp2p/p2p/host/routed"
+)
 
 /* BEGIN EXPORTED METHODS */
 
 // NewPseudoSub tests the functionality of the NewPseudoSub helper method.
 func TestNewPseudoSub(t *testing.T) {
-	// host, err := libp2p.New()
-	// sub, err := NewPseudoSub(host *routedhost.RoutedHost, opts ...Option)
+	ctx, cancel := context.WithCancel(context.Background()) // Initialize context
+
+	defer cancel() // Cancel
+
+	host, err := libp2p.New(
+		ctx,
+		libp2p.NATPortMap(),
+		libp2p.ListenAddrStrings(
+			"/ip4/0.0.0.0/tcp/1111",
+			"/ip6/::1/tcp/1111",
+		),
+	) // Initialize host
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	dht, err := dht.New(ctx, host) // Initialize dht
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	routedHost := routed.Wrap(host, dht) // Wrap host
+
+	sub, err := NewPseudoSub(routedHost) // Initialize sub
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	if sub == nil { // Check for errors
+		t.Fatal("Nil sub.") // Panic
+	}
 }
 
 /* END EXPORTED METHODS */
